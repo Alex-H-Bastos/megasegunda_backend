218b9da92dbe57d0479bce0e21295484b1b9a2dd
Alex-H-Bastos <alex.hbastos@sempreceub.com>
2024-11-04 10:09:29 -0300

    aa

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("ahaha");
+
+const express = require('express');
+const app = express();
+
+//app.Method('rota/caminho', (req, res) => {})
+app.get('/users', (req, res) => {
+    res.send('Rota para retornar usuarios');
+});
+
+const Port = 3000;
+
+app.listen(Port, () => {
+    console.log(`servidor web na porta ${Port}`);
+});
